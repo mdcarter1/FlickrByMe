0e710650899e34fb5130e12232f9e97253811d60
Mike Carter <emc045@motorola.com>
2015-08-11 16:34:08 -0400

    Added a new empty Universal Xcode project

---
+++ FlickrByMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrByMe
+//
+//  Created by Carter Mike-EMC045 on 8/11/15.
+//  Copyright (c) 2015 Mike Carter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrByMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrByMe
+//
+//  Created by Carter Mike-EMC045 on 8/11/15.
+//  Copyright (c) 2015 Mike Carter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
