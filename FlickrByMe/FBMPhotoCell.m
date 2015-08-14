9ae2a7059aa2a9fabc062a62b7f6abb1e9f7031b
Mike Carter <emc045@motorola.com>
2015-08-14 08:31:13 -0400

    Removed unused ViewController ref from storyboard and removed shadow on photo cells to make them draw faster

--- FlickrByMe/FBMPhotoCell.m
+++ FlickrByMe/FBMPhotoCell.m
@@ -16,15 +16,11 @@
   if (self) {
     self.backgroundColor = [UIColor colorWithWhite:0.85f alpha:1.0f];
     
-    self.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.layer.borderColor = [UIColor blackColor].CGColor;
     self.layer.borderWidth = 3.0f;
-    self.layer.shadowColor = [UIColor blackColor].CGColor;
-    self.layer.shadowRadius = 3.0f;
-    self.layer.shadowOffset = CGSizeMake(0.0f, 2.0f);
-    self.layer.shadowOpacity = 0.5f;
     
     self.imageView = [[UIImageView alloc] initWithFrame:self.bounds];
-    self.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
     self.imageView.clipsToBounds = YES;
     
     [self.contentView addSubview:self.imageView];
