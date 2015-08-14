e79caba2ad92a17c2d0f9af02b93ce14fa5afb17
Mike Carter <emc045@motorola.com>
2015-08-14 16:20:55 -0400

    Moved the FBMPhotViewVC activity indicator into the xib and use autolayout

--- FlickrByMe/FBMPhotoViewController.m
+++ FlickrByMe/FBMPhotoViewController.m
@@ -12,7 +12,7 @@
 @interface FBMPhotoViewController ()
 
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
-@property (nonatomic, strong) UIActivityIndicatorView *activityView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityView;
 
 @end
 
@@ -22,34 +22,20 @@
 {
   [super viewDidLoad];
   // Do any additional setup after loading the view from its nib.
+  
+  [self.activityView startAnimating];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before
-navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark - Property Overrides
 
 - (void)setEntry:(FBMPhotoEntry *)entry
 {
   _entry = entry;
-  
-  self.activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-  self.activityView.center = self.view.center;
-  [self.view addSubview:self.activityView];
-  [self.activityView startAnimating];
 
   // Lazy load the photo
   NSString *urlString =
       // Got this here: https://www.flickr.com/services/api/misc.urls.html
-      [NSString stringWithFormat:@"http://farm%ld.static.flickr.com/%ld/%lld_%@_d.jpg",
+      [NSString stringWithFormat:@"http://farm%ld.static.flickr.com/%ld/%lld_%@_h.jpg",
                                  (long)entry.farm, (long)entry.server, entry.photoId, entry.secret];
 
   NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
